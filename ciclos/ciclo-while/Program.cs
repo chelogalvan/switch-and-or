13aeac82df83b73905af21fa0b37179bb04a66d1
kratorp04dev <marcelopin.dev@gmail.com>
2024-09-26 10:35:14 -0300

    ejemplo ciclo while

---
+++ ciclos/ciclo-while/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ciclo_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, cont = 0;
+
+            Console.WriteLine("ingrese un numero");
+            n = int.Parse(Console.ReadLine());
+
+            while (n != 0)
+            {
+                cont++;
+                Console.WriteLine("ingrese un numero");
+            n = int.Parse(Console.ReadLine());
+
+            }
+            Console.WriteLine("Ingresaste " + cont + " numeros.");
+        }
+    }
+}
