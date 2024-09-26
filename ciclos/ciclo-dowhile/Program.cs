804f5aff7d2f1efe6a13c1d6ee41f3417378ee0c
kratorp04dev <marcelopin.dev@gmail.com>
2024-09-26 10:53:55 -0300

    ejemplo ciclo do-while

---
+++ ciclos/ciclo-dowhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ciclo_dowhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, cont = 0;
+
+            
+
+            do
+            {
+                
+                Console.WriteLine("ingrese un numero");
+                n = int.Parse(Console.ReadLine());
+                cont++;
+                
+            }while (n != 0);
+            Console.WriteLine("Ingresaste " + cont + " numeros.");
+        }
+    }
+}
