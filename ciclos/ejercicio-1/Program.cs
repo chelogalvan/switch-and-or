b4a2e712b689977a3fae2c1470225137a3650201
chelogalvan <chelogalvan@gmail.com>
2024-09-26 21:56:54 -0300

    solucion ejercicio-1

--- ciclos/ejercicio-1/Program.cs
+++ ciclos/ejercicio-1/Program.cs
@@ -6,7 +6,12 @@ namespace ejercicio_1
     {
         static void Main(string[] args)
         {
-            
+            int i = 10;
+
+            for(i = 1; i <=10;i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
